7049108f32fc8a8927a1d494b2cd42ecfb65608d
João Pedro <joaopjt@icloud.com>
2023-09-15 12:09:47 -0300

    added j() method values

--- src/index.js
+++ src/index.js
@@ -456,7 +456,7 @@ class Flashlight {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['people'],
       web: []
     };
     
@@ -472,19 +472,19 @@ class Flashlight {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['earth'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['disaster'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['fire'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
       }
     }
